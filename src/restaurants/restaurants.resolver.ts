07cd9bc204e8ddaf712da9ddd86b4c5ee6d63820
coolchaeyoung <ggodchae@naver.com>
2020-12-31 18:52:29 +0900

    DB config

---
+++ src/restaurants/restaurants.resolver.ts
@@ -0,0 +1,16 @@
+import { Resolver, Query, Args, Mutation } from '@nestjs/graphql';
+import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
+import { Restaurant } from './entities/restaurants.entity';
+
+@Resolver(of => Restaurant)
+export class RestaurantsResolver {
+    @Query(returns => [Restaurant])
+    restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
+        return [];
+    }
+
+    @Mutation(returns => Boolean)
+    createRestaurant(@Args() CreateRestaurantDto:CreateRestaurantDto): boolean {
+        return true;
+    }
+}
