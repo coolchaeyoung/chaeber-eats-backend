8a580b154bcab9ab31e87e9f9b97bb57847df595
coolchaeyoung <ggodchae@naver.com>
2021-01-09 02:44:13 +0900

    mailgun

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,6 +13,7 @@ import { JwtModule } from './jwt/jwt.module';
 import { jwtMiddleWare } from './jwt/jwt.middleware';
 import { AuthModule } from './auth/auth.module';
 import { Verification } from './users/entities/verification.entity';
+import { MailModule } from './mail/mail.module';
 
 @Module({
   imports: [
@@ -29,7 +30,10 @@ import { Verification } from './users/entities/verification.entity';
         DB_USERNAME: Joi.string().required(),
         DB_PASSWORD: Joi.string().required(),
         DB_NAME: Joi.string().required(),
-        SECRET_KEY: Joi.string().required()
+        SECRET_KEY: Joi.string().required(),
+        MAILGUN_API_KEY: Joi.string().required(),
+        MAILGUN_DOMAIN_NAME: Joi.string().required(),
+        MAILGUN_FROM_EMAIL: Joi.string().required(),
       })
     }),
     TypeOrmModule.forRoot({
@@ -52,7 +56,12 @@ import { Verification } from './users/entities/verification.entity';
     JwtModule.forRoot({
       privateKey: process.env.SECRET_KEY
     }),
-    AuthModule
+    AuthModule,
+    MailModule.forRoot({
+      apiKey:process.env.MAILGUN_API_KEY,
+      domain:process.env.MAILGUN_DOMAIN_NAME,
+      fromEmail:process.env.MAILGUN_FROM_EMAIL
+    })
   ],
   controllers: [],
   providers: [],
