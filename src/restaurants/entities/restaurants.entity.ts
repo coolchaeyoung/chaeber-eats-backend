8e8b990e7e3607c7dacf08bb66e20bcc6edee4db
coolchaeyoung <ggodchae@naver.com>
2021-01-16 23:37:47 +0900

    restaurant entity

--- src/restaurants/entities/restaurants.entity.ts
+++ src/restaurants/entities/restaurants.entity.ts
@@ -1,36 +1,32 @@
-import { Field, ObjectType } from "@nestjs/graphql";
-import { IsOptional, IsString, Length } from "class-validator";
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Field, ObjectType } from '@nestjs/graphql';
+import { IsOptional, IsString, Length } from 'class-validator';
+import { CoreEntity } from 'src/common/entities/core.entity';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @ObjectType()
 @Entity()
-export class Restaurant {
+export class Restaurant extends CoreEntity {
+  @Field((type) => String)
+  @Column()
+  @IsString()
+  @Length(5)
+  name: string;
 
-    @PrimaryGeneratedColumn()
-    @Field(type => Number)
-    id: number;
+  @Field((type) => Boolean, { nullable: true })
+  @Column({ default: true })
+  @IsOptional()
+  isVegan: boolean;
 
-    @Field(type => String)
-    @Column()
-    @IsString()
-    @Length(5)
-    name: string;
+  @Field((type) => String)
+  @Column()
+  @IsString()
+  address: string;
 
-    @Field(type => Boolean, { nullable: true})
-    @Column({ default: true })
-    @IsOptional()
-    isVegan: boolean;
+  @Field((type) => String)
+  @Column()
+  ownerName: string;
 
-    @Field(type => String)
-    @Column()
-    @IsString()
-    address: string;
-
-    @Field(type => String)
-    @Column()
-    ownerName: string;
-
-    @Field(type => String)
-    @Column()
-    categoryName: string;
+  @Field((type) => String)
+  @Column()
+  categoryName: string;
 }
